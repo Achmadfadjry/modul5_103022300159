da2ba204a5745099ec11aca55c3b958c54cf9fcc
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-18 07:40:44 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

---
+++ PemrosesData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300159
+{
+    internal class PemrosesData<T>
+    {
+        static void DapatkanNilaiTerbesar<T>(ref T lhs, ref T rhs)
+        {
+
+              T data;
+
+            PublicKey PemrosesData(T data)
+            {
+                this.data = data;
+            }
+
+            Public void PrintData()
+            {
+                Console.WriteLine("{data}");
+            }
+        }
+
+
+    }
+    class program
+    {
+        public static void Main()
+        {
+            DapatkanNilaiTerbesar<>
+        }
+    }
+
+    
+}
--- obj/Debug/net8.0/modul5_103022300159.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_103022300159.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_103022300159")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+899bbcd52d5099c72af182d0a162a346c9b08a39")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_103022300159")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_103022300159")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
